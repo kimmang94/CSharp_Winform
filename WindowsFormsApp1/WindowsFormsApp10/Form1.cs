8284530f3a147671bf9277a8ab177f07ce68cefe
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-26 09:41:08 +0900

    winform01

--- WindowsFormsApp1/WindowsFormsApp10/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp10/Form1.cs
@@ -15,11 +15,13 @@ namespace WindowsFormsApp10
     public partial class Form1 : Form
     {
         private SqlConnection sqlConn = null;
+        #region [DB 접속 정보]
         private string connStr = "SERVER=" + ConfigurationManager.AppSettings["IP"] + 
             ","+ ConfigurationManager.AppSettings["PORT"] + ";" + 
             "DATABASE="+ ConfigurationManager.AppSettings["DBNAME"] + ";" +
             "UID=" + ConfigurationManager.AppSettings["USERID"] + ";" +
             "PASSWORD=" + ConfigurationManager.AppSettings["USERPASSWORD"];
+        #endregion
         public Form1()
         {
             InitializeComponent();
@@ -32,9 +34,16 @@ namespace WindowsFormsApp10
         /// <param name="e"></param>
         private void btn1_Click(object sender, EventArgs e)
         {
-            sqlConn = new SqlConnection(connStr);
-            sqlConn.Open();
-            MessageBox.Show("데이터 베이스 연결 성공");
+            try
+            {
+                sqlConn = new SqlConnection(connStr);
+                sqlConn.Open();
+                MessageBox.Show("데이터 베이스 연결 성공");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("에러 발생 내용" + ex.ToString());
+            }
         }
 
         /// <summary>
@@ -46,11 +55,20 @@ namespace WindowsFormsApp10
         {
             // 데이터 베이스와 연결 끊기
             // null 이아니라면 연결중
-            if (sqlConn != null)
+            try
             {
-                sqlConn.Close();
-                MessageBox.Show("데이터 베이스 연결해제 성공");
+                if (sqlConn != null)
+                {
+                    sqlConn.Close();
+                    MessageBox.Show("데이터 베이스 연결해제 성공");
+                }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show("에러 발생 내용" + ex.ToString());
+            }
+
+            
         }
     }
 }
