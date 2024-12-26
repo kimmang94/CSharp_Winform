d004818c8d009ad435d4aa035799b93a5b84397b
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-26 16:32:49 +0900

    winform01

---
+++ WindowsFormsApp1/WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.gbox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gbox1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(1251, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(184, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "데이터베이스 연결";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(1251, 41);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(184, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "연결 끊기";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(1251, 70);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(184, 23);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "전체조회";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // gbox1
+            // 
+            this.gbox1.Controls.Add(this.textBox2);
+            this.gbox1.Controls.Add(this.textBox1);
+            this.gbox1.Controls.Add(this.lbl2);
+            this.gbox1.Controls.Add(this.lbl1);
+            this.gbox1.Controls.Add(this.btn4);
+            this.gbox1.Location = new System.Drawing.Point(1251, 111);
+            this.gbox1.Name = "gbox1";
+            this.gbox1.Size = new System.Drawing.Size(305, 131);
+            this.gbox1.TabIndex = 3;
+            this.gbox1.TabStop = false;
+            this.gbox1.Text = "책 삭제";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(154, 21);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(154, 21);
+            this.textBox1.TabIndex = 13;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(6, 60);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(81, 12);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "선택한 책이름";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(6, 30);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(63, 12);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "선택한 NO";
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(8, 92);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(184, 23);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "삭제";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Controls.Add(this.lbl4);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Location = new System.Drawing.Point(1251, 261);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 136);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "책이름 변경";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 21);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(154, 21);
+            this.textBox4.TabIndex = 15;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(6, 34);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(63, 12);
+            this.lbl3.TabIndex = 9;
+            this.lbl3.Text = "선택한 NO";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(6, 63);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(81, 12);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "새로운 책이름";
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(8, 99);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(184, 23);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "변경";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.lbl7);
+            this.groupBox2.Controls.Add(this.lbl6);
+            this.groupBox2.Controls.Add(this.lbl5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(1251, 412);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 171);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "신규책 추가";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(90, 97);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(154, 21);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(90, 66);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(154, 21);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(90, 39);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(154, 21);
+            this.textBox6.TabIndex = 17;
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(6, 100);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(41, 12);
+            this.lbl7.TabIndex = 12;
+            this.lbl7.Text = "책코드";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(6, 70);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(69, 12);
+            this.lbl6.TabIndex = 11;
+            this.lbl6.Text = "신규 책이름";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(6, 44);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(51, 12);
+            this.lbl5.TabIndex = 10;
+            this.lbl5.Text = "신규 NO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1206, 571);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1568, 600);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbox1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbox1.ResumeLayout(false);
+            this.gbox1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox gbox1;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConn = null;
+        #region [DB 접속 정보]
+        private string connStr = "SERVER=" + ConfigurationManager.AppSettings["IP"] +
+            "," + ConfigurationManager.AppSettings["PORT"] + ";" +
+            "DATABASE=" + ConfigurationManager.AppSettings["DBNAME"] + ";" +
+            "UID=" + ConfigurationManager.AppSettings["USERID"] + ";" +
+            "PASSWORD=" + ConfigurationManager.AppSettings["USERPASSWORD"];
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// DB 연결 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConn = new SqlConnection(connStr);
+                sqlConn.Open();
+                MessageBox.Show("데이터 베이스 연결 성공");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("에러 발생 내용" + ex.ToString());
+            }
+        }
+        /// <summary>
+        /// DB 해제 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlConn != null)
+                {
+                    sqlConn.Close();
+                    MessageBox.Show("데이터 베이스 연결해제 성공");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("에러 발생 내용" + ex.ToString());
+            }
+        }
+        /// <summary>
+        /// 전체 조회 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                //conn.Open() 코드를 안적어도 필요할 경우 연결 후 연결을 끊는다
+                //SqlDataAdapter 클래스를 이용하여 데이터 가져오기
+                string sql = "SELECT * FROM BOOKS(NOLOCK)";
+                SqlDataAdapter adpt = new SqlDataAdapter(sql, conn);
+                adpt.Fill(ds, "BOOKS");
+
+                //SqlDataReader 클래스를 이용하여 데이터 가져오기
+                //SqlCommand sqlCommand = new SqlCommand();
+                //sqlCommand.CommandText = "SELECT * FROM BOOKS";
+                //sqlCommand.Connection = conn;
+
+                //DataTable dataTable = new DataTable();
+                //SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                //dataTable.Load(sqlDataReader);
+                //dataGridView1.DataSource = dataTable;
+            }
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Book NO, Name 할당
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells["BOOKNO"].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells["NAME"].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells["BOOKNO"].Value.ToString();
+        }
+
+        /// <summary>
+        /// 삭제 기능
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string bookNo = textBox1.Text;
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = conn;
+                sqlCommand.CommandText = "DELETE FROM BOOKS WHERE BOOKNO = " + "'" + bookNo + "'";
+                sqlCommand.ExecuteNonQuery();
+                //btn4_Click(null, null);
+            }
+        }
+
+        /// <summary>
+        /// 책 이름 변경 기능
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            string bookNo = textBox4.Text;
+            string bookName = textBox3.Text;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = conn;
+                sqlCommand.CommandText = "UPDATE BOOKS SET NAME = " + "'" + bookName + "'" + "WHERE BOOKNO = " + bookNo;
+                sqlCommand.ExecuteNonQuery();
+                
+                //btn5_Click(null, null);
+            }
+        }
+
+        /// <summary>
+        /// 책 추가 기능
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string bookNo = textBox6.Text;
+                string bookName = textBox5.Text;
+                string bookCode = textBox7.Text;
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = "INSERT INTO BOOKS(BOOKNO,NAME,CODE) VALUES ( " + bookNo + ", '" + bookName + "','" + bookCode + "')"; 
+                sqlCommand.Connection = conn;
+                sqlCommand.ExecuteNonQuery();
+                //btn5_Click(null, null);
+            }
+        }
+    }
+}
